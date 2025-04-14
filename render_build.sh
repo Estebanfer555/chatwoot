8f131f81255a29204dca17fd09a500aa3ecc4b9a
Esteban Fernández <101114035+Estebanfer555@users.noreply.github.com>
2025-04-14 13:54:26 -0300

    Update render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
+set -e
 
-# Regenerar binstubs de Bundler
 bundle binstubs bundler --force
-
-# Preparar la base de datos (propio de Chatwoot)
-bundle exec rails db:chatwoot_prepare
-
-# Compilar los assets de Vite
-RAILS_ENV=production bin/vite build
+bundle install
+bundle exec rails db:prepare
+yarn install
+vite build
