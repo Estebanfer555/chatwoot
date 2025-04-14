ff976432566a0e25351315e3627e7244eda7ebc8
Esteban Fernández <101114035+Estebanfer555@users.noreply.github.com>
2025-04-14 13:58:03 -0300

    Update render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+echo "🔧 Preparando entorno Ruby"
 bundle binstubs bundler --force
 bundle install
+
+echo "🛠️  Preparando base de datos"
 bundle exec rails db:prepare
+
+echo "📦 Instalando dependencias JS"
 yarn install
+
+echo "⚡️ Build con Vite"
 vite build
